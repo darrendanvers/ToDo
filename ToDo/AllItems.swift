10ea08c5cee32a602f108aa1b39c1d73ac8ba483
Darren Danvers <darren@darrencodes.dev>
2022-07-14 16:29:31 -0500

    initial commit

---
+++ ToDo/AllItems.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToDo
+//
+//  Created by Darren Danvers on 7/14/22.
+//
+
+import SwiftUI
+
+struct AllItems: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct AllItems_Previews: PreviewProvider {
+    static var previews: some View {
+        AllItems()
+    }
+}
---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Darren Danvers on 7/14/22.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
